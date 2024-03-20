bee03ec596c58274b18a70b32937b1192d594ef2
Teplovozovich <artur.dmitriev.2016@inbox.ru>
2024-03-20 10:31:04 +0500

    feat: add formik

--- src/Components/Users/Users.tsx
+++ src/Components/Users/Users.tsx
@@ -4,6 +4,7 @@ import styles from "./Users.module.scss"
 import React from "react";
 import cn from "classnames"
 import { UserType } from "../../types/types.ts";
+import { Formik } from "formik";
 
 type PropsType = {
     totalUsersCount: number
@@ -30,7 +31,7 @@ let Users: React.FC<PropsType> = ({ users, totalUsersCount, pageSize, currentPag
                 pageSize={pageSize}
                 totalUsersCount={totalUsersCount}
                 onPageChanges={onPageChanges}
-                currentPage={currentPage} /  >
+                currentPage={currentPage} />
             {
                 users.map(data =>
                     <User id={data.id}
@@ -45,6 +46,67 @@ let Users: React.FC<PropsType> = ({ users, totalUsersCount, pageSize, currentPag
                     />)
             }
         </div >
+    )
+}
+
+const UsersSearchForm = () => {
+    return (
+        <div>
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                validate={values => {
+                    const errors = {};
+                    if (!values.email) {
+                        //errors.email = 'Required';
+                    } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ) {
+                        //errors.email = 'Invalid email address';
+                    }
+                    return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 400);
+                }}
+            >
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting,
+                    /* and other goodies */
+                }) => (
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="email"
+                            name="email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                        />
+                        {errors.email && touched.email && errors.email}
+                        <input
+                            type="password"
+                            name="password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                        />
+                        {errors.password && touched.password && errors.password}
+                        <button type="submit" disabled={isSubmitting}>
+                            Submit
+                        </button>
+                    </form>
+                )}
+            </Formik>
+
+        </div>
     )
 }
 
